db5ac5205cbdc4067a1f8ac5340ed36b5a751339
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-06-01 04:07:10 +0800

    Update msw_bot.py

--- msw_bot.py
+++ msw_bot.py
@@ -33,7 +33,9 @@ PLAYER_MAP = {
     "20372100005885364": {"name": "DCwaiting", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/506/1778862090312.png"},
     "20372100000737301": {"name": "HEE SABER", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/826/1747487981598.png"},
     "20372100004136222": {"name": "髒東西最愛", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/56/1779710669785.png"},
-    "20372100003917657": {"name": "雅", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/274/1744256438313.png"}
+    "20372100003917657": {"name": "大雅", "image": "https://mod-file.dn.nexoncdn.co.kr/profile/274/1744256438313.png"},
+    "20372100003500662": {"name": "雅", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/421/1742382425658.png"},
+    "20372100005520366": {"name": "伊卡路斯", "image": "https://mod-file.dn.nexoncdn.co.kr/shop/336/1749557991430.png"}
 }
 
 DEFAULT_IMAGE = "https://example.com/default.png"
