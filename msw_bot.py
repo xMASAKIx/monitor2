3a90b3f4dbd634c2bad39a31c49711c3eb97550d
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-09-21 20:40:14 +0800

    Update msw_bot.py

--- msw_bot.py
+++ msw_bot.py
@@ -31,7 +31,7 @@ PLAYER_MAP = {
 }
 
 DEFAULT_IMAGE = "https://example.com/default.png"
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1497592013166608484/-bQDkOKmZBbxRMXwkmgQqrFsk4cdrtKIuKfVlxk81XeXwqalZ-9VliOuSC5wI1YMcuRT"
+DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 
 # 建議調到 30 或 60 比較安全，但這邊先保留你原本的 15 試試看
 CHECK_INTERVAL = 15 
